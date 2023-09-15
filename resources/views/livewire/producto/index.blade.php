c22b24cd0f050c38bc7bff89053454a9076d9c53
Agustin <ageus94@gmail.com>
2023-09-15 09:27:07 -0600

    responsive tables producto

--- resources/views/livewire/producto/index.blade.php
+++ resources/views/livewire/producto/index.blade.php
@@ -8,7 +8,7 @@
 
         <!-- Botón de Agregar Producto -->
 
-        <div class="container d-flex justify-content-end p-0">
+        <div class="container d-flex p-0 justify-content-sm-end flex  justify-content-center mb-3">
             <button 
                 style="--bs-btn-font-weight: 600;" 
                 class="btn btn-success me-3" 
@@ -34,47 +34,49 @@
         </div>
 
         <!-- TABLA DE PRODUCTOS -->
-        <table class="table ">
-            <thead class="table-dark">
-                <tr>
-                <th class="text-center" scope="col">Código</th>
-                <th class="text-center" scope="col">Nombre</th>
-                <th class="text-center" scope="col">Precio(MX)</th>
-                <th class="text-center" scope="col">Stock</th>
-                <th class="text-center" scope="col">Agregó</th>
-                <th class="text-center" scope="col">Categoría</th>
-                <th class="text-center" scope="col">Código de Proveedor</th>
-                <th class="text-center" scope="col">Proveedor</th>
-                <th class="text-center" scope="col">Precio de Proveedor</th>
-                <th class="text-center" scope="col">Imagen</th>
-                <th style="text-align: center" scope="col">Acciones</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($productos as $producto)
-                <tr>
-                    <th class="text-center">{{$producto->id}}</th>
-                    <td class="text-center">{{$producto->name}}</td>
-                    <td class="text-center">{{$producto->precio_mx}}</td>
-                    <td class="text-center">{{$producto->stock}}</td>
-                    <td class="text-center">{{$producto->usuario->name}}</td>
-                    
-                    <td class="text-center">{{$producto->categoria->name ?? '-'}}</td>
-
-                    <td class="text-center">{{$producto->codigo_proveedor}}</td>
-                    <td class="text-center">{{$producto->proveedor}}</td>
-                    <td class="text-center">{{$producto->precio_proveedor}}</td>
-                    <td class="text-center">
-                        <img width="40px" height="40px" src="{{asset('/storage/images/productos/' . $producto->img_path)}}" alt="">
-                    </td>
-                    <td style="text-align: right;">
-                        <button wire:click="delete({{$producto->id}})" type="button" class="btn btn-outline-danger btn-sm">Borrar</button>
-                        <button wire:click="loadEditData({{$producto->id}})" data-bs-toggle="modal" data-bs-target="#editProducto" type="button" class="btn btn-outline-primary btn-sm">Editar</button>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+        <div class="table-responsive">
+            <table class="table ">
+                <thead class="table-dark">
+                    <tr>
+                    <th class="text-center" scope="col">Código</th>
+                    <th class="text-center" scope="col">Nombre</th>
+                    <th class="text-center" scope="col">Precio(MX)</th>
+                    <th class="text-center" scope="col">Stock</th>
+                    <th class="text-center" scope="col">Agregó</th>
+                    <th class="text-center" scope="col">Categoría</th>
+                    <th class="text-center" scope="col">Código de Proveedor</th>
+                    <th class="text-center" scope="col">Proveedor</th>
+                    <th class="text-center" scope="col">Precio de Proveedor</th>
+                    <th class="text-center" scope="col">Imagen</th>
+                    <th style="text-align: center" scope="col">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($productos as $producto)
+                    <tr>
+                        <th class="text-center">{{$producto->id}}</th>
+                        <td class="text-center">{{$producto->name}}</td>
+                        <td class="text-center">{{$producto->precio_mx}}</td>
+                        <td class="text-center">{{$producto->stock}}</td>
+                        <td class="text-center">{{$producto->usuario->name}}</td>
+                        
+                        <td class="text-center">{{$producto->categoria->name ?? '-'}}</td>
+    
+                        <td class="text-center">{{$producto->codigo_proveedor}}</td>
+                        <td class="text-center">{{$producto->proveedor}}</td>
+                        <td class="text-center">{{$producto->precio_proveedor}}</td>
+                        <td class="text-center">
+                            <img width="40px" height="40px" src="{{asset('/storage/images/productos/' . $producto->img_path)}}" alt="">
+                        </td>
+                        <td style="text-align: right;">
+                            <button wire:click="delete({{$producto->id}})" type="button" class="btn btn-outline-danger btn-sm">Borrar</button>
+                            <button wire:click="loadEditData({{$producto->id}})" data-bs-toggle="modal" data-bs-target="#editProducto" type="button" class="btn btn-outline-primary btn-sm">Editar</button>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
         {{ $productos->links() }}
 
     </div>
