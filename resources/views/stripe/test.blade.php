9e3bc19054fcb00dc6a50eb0da0e113a63e0afac
Agustin <ageus94@gmail.com>
2023-10-26 15:32:18 -0600

    formularios mejorados (bonitos)

--- resources/views/stripe/test.blade.php
+++ resources/views/stripe/test.blade.php
@@ -1,32 +1,45 @@
-<style>
-    .text{
-        font-size: 3rem;
-        color: greenyellow;
-        font-weight: bold;
-    }
-    .link > a{
-        color: white;
-        font-size: 2rem;
-    }
-    .full_container {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-        background-color: black;
-        flex-direction: column
-    }
-    body {
-        margin: 0;
-        padding: 0;
-    }
-</style>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <style>
+        .text{
+            font-size: 3rem;
+            color: greenyellow;
+            font-weight: bold;
+        }
+        .link > a{
+            color: white;
+            font-size: 2rem;
+        }
+        .full_container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: black;
+            flex-direction: column
+        }
+        body {
+            margin: 0;
+            padding: 0;
+        }
+    </style>
+    
+    <div class="full_container">
+        <div class="text">
+           ¡Pago realizado correctamente!
+        </div>
+        <div class="link">
+            <a href="{{route('MisCompras')}}">Regresar a mis compras</a>
+        </div>
+    </div>
+</body>
+</html>
 
-<div class="full_container">
-    <div class="text">
-       ¡Pago realizado correctamente!
-    </div>
-    <div class="link">
-        <a href="{{route('MisCompras')}}">Regresar a mis compras</a>
-    </div>
-</div>
+
