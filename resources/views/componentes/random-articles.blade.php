308f039c78adb1a8535689270809f7a3de76d492
Ageus <ageus94@gmail.com>
2023-11-10 16:24:04 -0600

    a

--- resources/views/componentes/random-articles.blade.php
+++ resources/views/componentes/random-articles.blade.php
@@ -3,8 +3,8 @@
         display: flex;
         width: 95%;
         margin-inline: auto;
-        gap: 0.5rem
-
+        gap: 0.5rem;
+        margin-block: 5rem;
     }
     .producto-nonSlider-card {
         flex: 1;
@@ -17,12 +17,21 @@
         width: 100%;
         display:flex;
         justify-content: center;
+        padding-top: 3rem;
+        padding-bottom: 1rem;
     }
     .producto-nonSlider-card-img img{
         width: 90%
     }
     .producto-nonSlider-card-text{
         padding-inline: 1rem;
+        border-top: 1px solid rgba(0, 0, 0, 0.4);
+        padding-block: 1rem;
+    }
+    .producto-nonSlider-card-text .precio-producto{
+        color: red;
+        font-size: 1.5rem; 
+        font-weight: bold;
     }
 </style>
 
@@ -36,6 +45,11 @@
             </div>
 
             <div class="producto-nonSlider-card-text">
+
+                <div class="precio-producto">
+                    <div> ${{ $nonSliderArticle->precio_mx }} mx </div>
+                </div>
+
                 <div class="nombre-producto">
                     <div> {{ $nonSliderArticle->name }} </div>
                 </div>
