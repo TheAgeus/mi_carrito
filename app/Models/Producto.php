6cd82fa6fae1f16a2e56230a53f7e2135e6c1eb9
Agustin <ageus94@gmail.com>
2023-11-14 13:42:37 -0600

    funcion promedio de las calificaciones

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -23,8 +23,26 @@ class Producto extends Model
     ];
 
     public function average_score()
-    {
-        return 5;
+    {   
+        $current_id = $this->id;
+
+        $all_rates = Comentario::where('producto_id', $current_id)->get('calificacion');
+
+        if(count($all_rates) > 0){
+            $sumatoria = 0;
+            $contador = 0;
+    
+            foreach ($all_rates as $rate) {
+                $sumatoria = $sumatoria + $rate->calificacion;
+                $contador = $contador + 1;
+            }
+    
+            $total = $sumatoria / $contador;
+    
+            return (int)round($total);
+        }
+        return 0;
+
     }
 
     public function usuario()
